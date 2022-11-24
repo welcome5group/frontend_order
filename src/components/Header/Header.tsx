6d9a201712291994cf662977a1fc28a44670cca0
jsy9049 <kouawb102@naver.com>
2022-11-24 19:54:22 +0900

    Feat: #4 - 헤더 컴포넌트 개발

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from './Header.module.scss'
 import { AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.svg'
 
 const Header = () => {
   return (
@@ -9,6 +10,9 @@ const Header = () => {
       <div>
         <AiOutlineMenu className={styled.menu} />
       </div>
+      <Link to='/' className={styled.logoLink}>
+        <img src={logo} className={styled.logo} alt="로고" />
+      </Link>
       <div>
         <p className={styled.loginBtn}><Link to="/login">로그인</Link></p>
       </div>
