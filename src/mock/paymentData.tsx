f080edbb9ed5156904ca81d61ea71ae2a635c46a
jsy9049 <kouawb102@naver.com>
2022-12-06 16:47:23 +0900

    Feat: #17 - 테스트 데이터 생성

---
+++ src/mock/paymentData.tsx
@@ -0,0 +1,47 @@
+export const paymentData = [
+  {
+    id: 1,
+    date: '2022-01-10',
+    storeName: "김밥집",
+    menu: [{
+      menuName: "김밥",
+      count: 3,
+      price: 1500,
+    }, {
+      menuName: "떡볶이",
+      count: 1,
+      price: 2000,
+    }],
+    totalPrice: 6500
+  },
+  {
+    id: 2,
+    date: '2022-01-12',
+    storeName: "피자집",
+    menu: [{
+      menuName: "피자",
+      count: 3,
+      price: 15000,
+    }, {
+      menuName: "떡볶이",
+      count: 1,
+      price: 2000,
+    }],
+    totalPrice: 47000
+  },
+  {
+    id: 3,
+    date: '2022-02-10',
+    storeName: "치킨집",
+    menu: [{
+      menuName: "치킨",
+      count: 3,
+      price: 15000,
+    }, {
+      menuName: "떡볶이",
+      count: 1,
+      price: 2000,
+    }],
+    totalPrice: 47000
+  },
+]
