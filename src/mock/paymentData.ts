6b76b6811969f81d012247ec9b7fb18afbf62979
jsy9049 <kouawb102@naver.com>
2022-12-21 22:48:39 +0900

    Feat: #17 - 테스트 데이터 재확인

--- src/mock/paymentData.ts
+++ src/mock/paymentData.ts
@@ -1,92 +1,110 @@
 export const paymentData = [
   {
     id: 1,
-    date: '2022-01-10',
+    date: "2022-01-10",
     storeName: "김밥집",
-    menu: [{
-      menuName: "김밥",
-      count: 3,
-      price: 1500,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 6500
+    menu: [
+      {
+        menuName: "김밥",
+        count: 3,
+        price: 1500,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 6500,
   },
   {
     id: 2,
-    date: '2022-01-12',
+    date: "2022-01-12",
     storeName: "피자집",
-    menu: [{
-      menuName: "피자",
-      count: 3,
-      price: 15000,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 47000
+    menu: [
+      {
+        menuName: "피자",
+        count: 3,
+        price: 15000,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 47000,
   },
   {
     id: 3,
-    date: '2022-02-11',
+    date: "2022-02-11",
     storeName: "치킨집",
-    menu: [{
-      menuName: "치킨",
-      count: 3,
-      price: 15000,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 47000
+    menu: [
+      {
+        menuName: "치킨",
+        count: 3,
+        price: 15000,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 47000,
   },
   {
     id: 3,
-    date: '2022-03-01',
+    date: "2022-03-01",
     storeName: "치킨집",
-    menu: [{
-      menuName: "치킨",
-      count: 3,
-      price: 15000,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 47000
+    menu: [
+      {
+        menuName: "치킨",
+        count: 3,
+        price: 15000,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 47000,
   },
   {
     id: 4,
-    date: '2022-03-01',
+    date: "2022-03-01",
     storeName: "치킨집",
-    menu: [{
-      menuName: "치킨",
-      count: 3,
-      price: 15000,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 47000
+    menu: [
+      {
+        menuName: "치킨",
+        count: 3,
+        price: 15000,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 47000,
   },
   {
     id: 5,
-    date: '2021-03-01',
+    date: "2021-03-01",
     storeName: "치킨집",
-    menu: [{
-      menuName: "치킨",
-      count: 3,
-      price: 15000,
-    }, {
-      menuName: "떡볶이",
-      count: 1,
-      price: 2000,
-    }],
-    totalPrice: 47000
+    menu: [
+      {
+        menuName: "치킨",
+        count: 3,
+        price: 15000,
+      },
+      {
+        menuName: "떡볶이",
+        count: 1,
+        price: 2000,
+      },
+    ],
+    totalPrice: 47000,
   },
-]
+];
