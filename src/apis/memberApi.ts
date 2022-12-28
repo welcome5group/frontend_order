8bd02e3ae51a8e47207d8258a4ac7facbc72e8bf
jsy9049 <kouawb102@naver.com>
2022-12-29 00:03:49 +0900

    배포ㅠㅠ

--- src/apis/memberApi.ts
+++ src/apis/memberApi.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { PROXY } from "./proxy";
 
 interface signupType {
   email: string;
@@ -51,7 +50,7 @@ const signCheck = async (uuid: string) => {
 
 //로그인 API
 const signIn = async (inputValue: signInType) => {
-  const res = await axios.post(`${PROXY}/api/auth/sign-in`, inputValue);
+  const res = await axios.post(`/api/auth/sign-in`, inputValue);
 
   return res;
 };
